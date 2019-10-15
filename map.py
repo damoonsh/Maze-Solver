f4d7b23560c6a793d36ad5f8ec8ae47b9ac97a74
Damoon <d.shahhosseini@yahoo.com>
2019-10-15 16:47:35 -0400

    Some changes and modifications are needed to make a better communication between the mover

--- map.py
+++ map.py
@@ -8,12 +8,12 @@ from mover import Mover
 
 class Map:
     # Initializing the Object with the default values for rows, cols and scale
-    def __init__(self, cols=10, rows=10, scale=40):
-        self.row = rows
-        self.col = cols
+    def __init__(self, scale=40):
+        self.row = 10
+        self.col = 10
         self.scale = scale
         self.blocks = []
-        self.diff = 0.4
+        self.diff = 0.45
 
     # Runs the whole app
     def run(self):
@@ -25,6 +25,7 @@ class Map:
 
         # Drawing the map
         self.obj = Mover()
+        self.obj.set_scale(self.scale)
         self.maze()
         self.generate_map()
 
@@ -47,7 +48,7 @@ class Map:
     def maze(self):
         num = int((self.row * self.col) * self.diff)
         for i in range(num):
-            self.blocks.append(((random.randrange(9) + 1), (random.randrange(9) + 1)))
+            self.blocks.append(((random.randrange(10)), (random.randrange(10))))
 
     # Block: Draws blocks pixel by pixel
     def block(self, x, y, color=Consts.White):
@@ -55,16 +56,31 @@ class Map:
             for c in range(self.scale):
                 self.pixel[x + r][y + c] = color
 
+    """Logic related functions: """
+    # This for the manual movement
     def move(self, dx, dy):
         # Setting some variables, so we can check if the movement makes sense or not
         out_range_x = (self.obj.x + self.scale * dx >= 0 and self.obj.x + self.scale * dx < self.scale * self.col)
         out_range_y = (self.obj.y + self.scale * dy >= 0 and self.obj.y + self.scale * dy < self.scale * self.row)
         out_range = out_range_x and out_range_y
+
         # At the beginning it should be checked to see if the co-ordinates are in the demanded range so we won't
         # have any, errors regarding the function not being in the range
         if out_range:
             # After checking to see for the co-ordinates it is up to the mover object to decide the movement
-            if self.obj.should_move(self.pixel[self.obj.x + self.scale * dx][self.obj.y + self.scale * dy]):
+            if self.pixel[self.obj.x + self.scale * dx][self.obj.y + self.scale * dy] != 6556180:
                 self.block(self.obj.x, self.obj.y)
                 self.obj.set_coordinates(self.obj.x + self.scale * dx, self.obj.y + self.scale * dy)
                 self.block(self.obj.x, self.obj.y, Consts.Item)
+
+    # This for general movement
+    def autoMove(self):
+        if self.obj.move_logic(self.pixel, self.obj.x, self.obj.y):
+            x, y = self.obj.move_logic(self.pixel, self.obj.x, self.obj.y)
+            self.block(self.obj.x, self.obj.y)
+            self.obj.set_coordinates(x, y)
+            self.block(self.obj.x, self.obj.y, Consts.Item)
+            self.obj.visited_coordinates.append((x, y))
+            pygame.time.wait(1000)
+        else:
+            print('The end')
