e625acd8f4021a939f073862cacc53edf7ea3537
Damoon <d.shahhosseini@yahoo.com>
2019-10-16 15:59:33 -0400

    Added a new feature to better communicate with the modules

--- Consts.py
+++ Consts.py
@@ -3,3 +3,12 @@
 Black = (100, 10, 20)
 White = (125, 250, 25)
 Item = (30, 200, 90)
+
+class vals:
+    def set_props(self, scale=10, rows=10, cols=40):
+        self.scale = scale
+        self.row = rows
+        self.col = cols
+
+    def __str__(self):
+        print(f'col: {self.col}, row: {self.row}, scale: {self.scale}')
