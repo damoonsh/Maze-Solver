27752d407ee6143cfb9055609df02bede5da558b
Damoon <d.shahhosseini@yahoo.com>
2019-10-18 22:49:22 -0400

    Having some problems, a test should be written

--- Tracker.py
+++ Tracker.py
@@ -2,13 +2,12 @@
     This object is going to track the movements of the object and then give the necessary data
     when needed
 '''
-class Movement():
-    def __int__(self, type, fromCoor, toCoor, bef="", aft=""):
+class Movement:
+    # Initializes the object with the given set of properties
+    def __int__(self, type, fromCoor, toCoor):
         self.type = type
         self.From = fromCoor
         self.To  = toCoor
-        self.before = bef
-        self.after = aft
     def set_before(self, obj):
         self.before = obj
     def set_after(self, obj):
