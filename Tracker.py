1e4206c0a4af255c7f27ea12ae6b7fa054c87775
Damoon <d.shahhosseini@yahoo.com>
2019-10-25 14:51:31 -0400

    Adding the tracker functionality

--- Tracker.py
+++ Tracker.py
@@ -1,11 +1,17 @@
 '''
-    This object is going to track the movements of the object and then give 
+    This object is going to track the movements of the object and then give
     the necessary data when needed.
 '''
 
 
 class Move:
     def set_props(self, props):
-        self.From = props.from_coor
-        self.To = props.to_coor
-        self.type = props.type
+        self.From = props["from_coor"]
+        self.To = props["to_coor"]
+        self.Type = props["move_type"]
+
+    def set_after(self, after):
+        self.after = after
+
+    def set_before(self, before):
+        self.before = before
