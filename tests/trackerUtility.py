7eb81cca1c1d51492dacbb796d9492f86605da77
Damoon <d.shahhosseini@yahoo.com>
2019-10-25 00:38:19 -0400

    Testing the tracking utility

---
+++ tests/trackerUtility.py
@@ -0,0 +1,18 @@
+'''
+    This file is used to check to see if the tracking on the obejct's movement 
+    is being done properly or not
+'''
+
+
+
+
+# The testing area
+import unittest
+
+class myTests(unittest.TestCase):
+    def test00(self):
+        pass   
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
