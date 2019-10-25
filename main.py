d315d9105f39044ce11e27150af50084215eb483
Damoon <d.shahhosseini@yahoo.com>
2019-10-25 16:11:33 -0400

    Add the part when it actually reaches to its place

--- main.py
+++ main.py
@@ -2,36 +2,39 @@ from map import Map
 import pygame
 from Consts import vals
 
+
 def main():
     # Initial boolean variable for the whole thing
     Run, pause = True, False
+
     # The function that prompts the user to enter the map properties
-    def set_vals():
+    def set_val():
         # Initializing an object with it's default values
         val = vals()
         val.set_props()
         # Prompting the user, if they want to adjust the map properties
-        command = input('Press any key but D/d to set your map settings: ')
+        comm = input('Press any key but D/d to set your map settings: ')
         # If the adjusting was requested then:
-        if command.lower() == 'd':
+        if comm.lower() == 'd':
             print('[MESSAGE]:You have chosen to set your map properties')
             # Get the values
             col = int(input('[INPUT]Enter the number of cols: '))
             row = int(input('[INPUT]Enter the number of rows: '))
-            scale = int(input('[INPUT]:Enter the scale of your map: '))
+            scale = int(input('[INPUT]Enter the scale of your map: '))
             val.set_props(scale=scale, rows=row, cols=col)
         # Return the val object
         return val
+
     # Get the consts object so other objects can communicate with each other easier
-    consts = set_vals()
+    consts = set_val()
     # Instantiate and run the map by the given consts
-    map = Map(consts)
-    map.run()
+    maze = Map(consts)
+    maze.run()
     # Main loop where the whole app runs
     while Run:
         # If it wasn't paused then the moving shall be proceeded
         if not pause:
-            map.autoMove()
+            maze.autoMove()
         """ Control section: """
         # Controlling the events within the app by this part
         for event in pygame.event.get():
@@ -49,17 +52,18 @@ def main():
                     command = input('Press y/Y if you want to change the delay time: ')
                     if command.lower() == 'y':
                         new_delay = int(input('New delay time: '))
-                        map.delay = new_delay
+                        maze.delay = new_delay
                         # Adjusting for the map properties if demanded
-                        consts = set_vals()
+                        consts = set_val()
                     # Re-instantiating the map and running it
-                    map = Map(consts)
-                    map.run()
+                    maze = Map(consts)
+                    maze.run()
 
                 if event.key == pygame.K_r:
                     # Re-instantiating the map and running it
-                    map = Map(consts)
-                    map.run()
+                    maze = Map(consts)
+                    Paused = False
+                    maze.run()
 
                 # For pausing the process
                 if event.key == pygame.K_x:
@@ -68,16 +72,17 @@ def main():
 
                 # Moving manually
                 if event.key == pygame.K_RIGHT:
-                    map.move(1, 0)
+                    maze.move(1, 0)
                 if event.key == pygame.K_LEFT:
-                    map.move(-1, 0)
+                    maze.move(-1, 0)
                 if event.key == pygame.K_DOWN:
-                    map.move(0, 1)
+                    maze.move(0, 1)
                 if event.key == pygame.K_UP:
-                    map.move(0, -1)
+                    maze.move(0, -1)
 
         # Updating the display
         pygame.display.update()
+
 
 if __name__ == '__main__':
     main()
