3219f824a6dc51787feeada4656974e3cec56f27
Damoon <d.shahhosseini@yahoo.com>
2019-11-29 01:59:26 -0500

    The working process for these wil be started!

--- Stimulator.py
+++ Stimulator.py
@@ -1,5 +1,7 @@
 """
     This file contains the modules needed to
     test/debug and examine the thought process being
-    implemented on the moving Object
+    implemented on the moving Object, thei module renders
+    the visualizer so it can be analyzed based on the data
+    obtained by the actual  moving object.
 """
