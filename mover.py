98373b45255b65f549489439425f3992067bff3a
Damoon <d.shahhosseini@yahoo.com>
2019-11-30 01:45:21 -0500

    Changing the structure in a more neater way.

--- mover.py
+++ mover.py
@@ -1,10 +1,11 @@
 """
     Contains the logic for the mover object
 """
-
+from utilities.Consts import directions, available_moves
 class Mover:
     # Instntiating the object at the origin/start point of its journey
-    def __init__(self):
+    def __init__(self, pixel):
+        self.pixelC = pixel
         # Initial co-ordinates for the mover object which is (0, 0)
         self.x = 0
         self.y = 0
@@ -26,41 +27,46 @@ class Mover:
     def set_dimensions(self, col, row):
         self.col = col
         self.row = row
-    # Helpers--------------------------------------------------------------------------
+    # Helpers-------------------------------------------------------------------
+    # Changing the values for different moves
+    def available_moves(self):
+        # There two possible scenarios: either it is the first movement which
+        # the list should be instantiated or it is not the first movement sot
+        # the list should be resseted.
+        self.reset()
+        for dir in possible_directions:
+            x, y = coordinate_change(dir)
+            if check_range(x, y) and pixelC[x][y] != 6556180:
+                self.possible_moves[dir] = True
+    # Returns the new coordinate for a specific movement
     def coordinate_change(self, key):
-        directions = {
-            "right": self.x + self.scale, self.y,
-            "left": self.x - self.scale, self.y,
-            "up": self.x, self.y - self.scale,
-            "down": self.x, self.y + self.scale
-        }
-        
         return directions[key]
+    # Checks to see if the point is in range and not dark
+    def validate_point(self):
+        pass
+    # Checks to see if the point is in the grid
+    def check_range(self, x, y):
+        if (0 <= x < self.scale * self.col) and (0 <= y < self.scale * self.row):
+            return True
+        return False
+    # Resets the available moves
+    def reset(self):
+        self.possible_moves = {
+            "right": False,
+            "left": False,
+            "up": False,
+            "down": False
+        }
     # --------------------------------------------------------------------------
     # Logic
-    def move_logic(self, pixelC, x, y):
+    def move_logic(self, x, y):
         # For each set of movement, first we add the movement to the next
         # movement of the current movement and then we equal the current
         # movement to the new movement.
         # ----------------------------------------------------------------------
         # Note: 6556180 is the color of the blocks
         # The default moves with the priority are: 1. Down 2. Left 3. Left 4. Up
-        # DOWN :
-        if self.check_range(x, y + self.scale) and self.point_confirm(x, y + self.scale):
-            if pixelC[x][y + self.scale] != 6556180:
-                return x, y + self.scale
-        # RIGHT :
-        if self.check_range(x + self.scale, y) and self.point_confirm(x + self.scale, y):
-            if pixelC[x + self.scale][y] != 6556180:
-                return x + self.scale, y
-        # LEFT :
-        if self.check_range(x - self.scale, y) and self.point_confirm(x - self.scale, y):
-            if pixelC[x - self.scale][y] != 6556180:
-                return x - self.scale, y
-        # UP:
-        if self.check_range(x, y - self.scale) and self.point_confirm(x, y - self.scale):
-            if pixelC[x][y - self.scale] != 6556180:
-                return x, y - self.scale
+        pass
         """
             If there were no movement available then the specific point is a dead
             end and also that point should be considered a dead end so it should
@@ -69,10 +75,6 @@ class Mover:
         """
         self.visited_coordinates.append((x, y))
 
-    def check_range(self, x, y):
-        if (0 <= x < self.scale * self.col) and (0 <= y < self.scale * self.row):
-            return True
-        return False
 
     # Checks to see if the object should proceed to a point or not
     def point_confirm(self, x, y):
