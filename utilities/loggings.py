203d5aad406056ed3818fb41115ba70874948fdb
Damoon <d.shahhosseini@yahoo.com>
2019-12-09 22:08:15 -0500

    Changed some functions in order to ease the debugging process

---
+++ utilities/loggings.py
@@ -0,0 +1,10 @@
+"""
+    Contains a set of loggins for the user interaction
+"""
+col_input = '[INPUT]Enter the number of cols: '
+row_input = '[INPUT]Enter the number of rows: '
+scale_input = '[INPUT]Enter the scale of your map: '
+map_settings = 'Press any key but D/d to set your map settings: '
+quit_message = 'Quiting the trial as demanded.'
+delay_adjusment = 'Press y/Y to change the delay time: '
+map_reset = '_____________Map is resetted_____________'
