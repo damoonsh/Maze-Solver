83828c8099b4b04731e806b0c2a32ffdb145683c
Damoon <d.shahhosseini@yahoo.com>
2020-02-01 02:14:37 -0500

    Feb 1th

--- tests/path.py
+++ tests/path.py
@@ -1,11 +1,42 @@
 import sqlite3
 from datetime import datetime
+
+# The db manager
+class dbManage:
+    def __init__(self, dbFile='test.db'):
+        self.conn = sqlite3.connect(self.dbFile)
+        self.c = conn.cursor()
+
+    def getDate():
+        return datetime.date(datetime.now()).year, datetime.date(datetime.now()).day, datetime.date(datetime.now()).month
+
 conn = sqlite3.connect('test.db')
 c = conn.cursor()
-# c.execute('''CREATE TABLE Deadends (col, row, number of moves, options, paths tried)''')
-
+# c.execute(
+#     '''CREATE TABLE Test (
+#     col INTEGER,
+#     row INTEGER,
+#     x INTEGER,
+#     y INTEGER,
+#     number of moves INTEGER,
+#     options INTEGER,
+#     taken_options VARCHAR(4),
+#     id VARCHAR(16),
+#     year INTEGER,
+#     month INTEGER,
+#     day INTEGER)
+# ''')
+# sql_command = """CREATE TABLE emp (
+# staff_number INTEGER PRIMARY KEY,
+# fname VARCHAR(20),
+# lname VARCHAR(30),
+# gender CHAR(1),
+# joining DATE);"""
 # Insert a row of data
-c.execute("INSERT INTO Deadends VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+year, day, month = getDate()
+x, y, cols, rows, idd = 100, 200, 30, 40, 'aafwfa'
+command = f'INSERT INTO Test VALUES ({cols}, {rows}, {x}, {y}, 2, 3, LR, {idd}, {year}, {month}, {day})'
+c.execute(command)
 
 # Save (commit) the changes
 conn.commit()
